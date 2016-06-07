f3d862bb9368b3aac6681a4c6df739ef452b2cac
kirillin <kaartemov@gmail.com>
2016-06-08 00:37:41 +0600

    DONT WORK, SKA!

--- courses/cpp/cycles/1137_elements_more_that_prev.cpp
+++ courses/cpp/cycles/1137_elements_more_that_prev.cpp
@@ -11,18 +11,24 @@ using namespace std;
 int main() {
   ifstream in("input.txt");
   ofstream out("output.txt");
-  int k, first, length;
-  first = 1;
+  int k, prev, length, max_sub_length;
+  max_sub_length = 0;
   length = 0;
-  do {
-    in >> k;
-    if (k > first) {
-      length++;
-
-    } else {
-      first = k;
-    }
-  } while (k != 0);
-  out << length << endl;
-	return 0;
+  in >> prev;
+  if (prev != 0) { // lol. im a stupid dude. ha-ha!
+    do {
+      in >> k;
+        if (k > prev) {
+          length++;
+        } else {
+          prev = k;
+          if (max_sub_length <= length) {
+            max_sub_length = length;
+            length = 0;
+          }
+        }
+    } while (k != 0);
+  }
+  out << max_sub_length << endl;
+  return 0;
 }
