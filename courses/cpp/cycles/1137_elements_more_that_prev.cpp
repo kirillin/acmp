1c9b968f1da09273b74db5ea5def431fd8cc6f2d
kirillin <kaartemov@gmail.com>
2016-06-08 16:40:36 +0600

    new version, but it dont work too :(

--- courses/cpp/cycles/1137_elements_more_that_prev.cpp
+++ courses/cpp/cycles/1137_elements_more_that_prev.cpp
@@ -5,30 +5,25 @@
   Artemov Kirill
 */
 #include <fstream>
-
+ 
 using namespace std;
-
+ 
 int main() {
   ifstream in("input.txt");
   ofstream out("output.txt");
-  int k, prev, length, max_sub_length;
-  max_sub_length = 0;
+  int k, first, length;
+  in >> first;
   length = 0;
-  in >> prev;
-  if (prev != 0) { // lol. im a stupid dude. ha-ha!
+  if (first != 0) {
     do {
       in >> k;
-        if (k > prev) {
-          length++;
-        } else {
-          prev = k;
-          if (max_sub_length <= length) {
-            max_sub_length = length;
-            length = 0;
-          }
-        }
+      if (k > first) {
+        length++;
+      } else if (k < first){
+        first = k;
+      }
     } while (k != 0);
   }
-  out << max_sub_length << endl;
-  return 0;
+  out << length << endl;
+    return 0;
 }
